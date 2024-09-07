d2c0fa605264546e14ee67a69bf7331ca496238c
NAWAZ-SRM <sheriffnawaz641@gmail.com>
2024-09-07 13:53:30 +0530

    version of model running on destination names

--- server/app.py
+++ server/app.py
@@ -1,6 +1,36 @@
+# from flask import Flask, request, jsonify
+# import pickle
+# import numpy as np
+
+# app = Flask(__name__)
+
+# with open('weights.pkl', 'rb') as mfile:
+#     model = pickle.load(mfile)
+
+
+# @app.route('/api/predict', methods=['POST'])
+# def predict():
+#     data = request.json
+#     print("Received data:", data)
+#     weight = float(data['weight'])
+#     distance = float(data['distance'])
+#     # loading_d = float(data['loading_meter'])
+#     loading_d = 0.000195
+
+#     features = np.array([[weight, loading_d, distance]])
+#     features = features.astype(float)
+#     results = model.predict(features)
+#     return jsonify({'predictions': float(results[0])})
+
+
+# if __name__ == '__main__':
+#     app.run(port=5001)
+
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
 
 app = Flask(__name__)
 
@@ -8,20 +38,39 @@ with open('weights.pkl', 'rb') as mfile:
     model = pickle.load(mfile)
 
 
+def calculate_distance(origin, destination):
+    loc = Nominatim(user_agent="GetLoc")
+    origin_location = loc.geocode(origin)
+    if origin_location is None:
+        raise AttributeError(f"Origin '{origin}' could not be found.")
+    destination_location = loc.geocode(destination)
+    if destination_location is None:
+        raise AttributeError(f"Destination '{destination}' could not be found.")
+    o_coords = (origin_location.latitude, origin_location.longitude)
+    d_coords = (destination_location.latitude, destination_location.longitude)
+    return round((geodesic(o_coords, d_coords).km), 3)
+
+
+@app.errorhandler(400)
+def handle_400_error(error):
+    return jsonify({'error': 'Bad request', 'message': str(error)}), 400
+
+
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    data = request.json
-    print("Received data:", data)
-    weight = float(data['weight'])
-    distance = float(data['distance'])
-    # loading_d = float(data['loading_meter'])
-    loading_d = 0.000195
-
-    features = np.array([[weight, loading_d, distance]])
-    features = features.astype(float)
-    results = model.predict(features)
-    return jsonify({'predictions': float(results[0])})
+    try:
+        data = request.get_json()
+        weight = float(data['weight'])
+        origin = data['origin']
+        destination = data['destination']
+        distance = calculate_distance(origin, destination)
+        loading_d = 0.000195  # data['loading_meters']
+        features = np.array([[weight, loading_d, distance]])
+        results = model.predict(features)
+        return jsonify({'predictions': round(float(results[0]), 3)})
+    except (ValueError, AttributeError) as e:
+        return handle_400_error(e)
 
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(debug=True, port=5001)
